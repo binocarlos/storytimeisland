259eab2cd63a50dac5aa8f2d5d4ce1e4785306ec
Kai Davenport <kaiyadavenport@gmail.com>
2013-06-01 13:58:17 +0000

    changed index.js to accept single argument [port]

--- index.js
+++ index.js
@@ -10,97 +10,69 @@ var url = require('url');
 var dye = require('dye');
 var fs = require('fs');
 
+var port = process.argv[2] || 8001;
 
-var program = require('commander');
-var version = require('./package.json').version;
+// create a spanking brand new Express App
+// it will save Sessions into Redis and so will survive a crash/restart
+var document_root = __dirname + '/www';
 
-program
-  .option('-p, --port <number>', 'port to listen on', '80')
-  .version(version)
-  
-program
-  .command('start')
-  .description('start the storytimeisland web server')
-  .action(function(env){
+var app = express();
+var server = http.createServer(app);
 
-  	var port = program.port || 80;
+app.use(express.favicon());
+app.use(express.query());
+app.use(express.bodyParser());
 
-  	runserver(port);
-  })
+app.engine('ejs', engines.ejs);
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
-program
-  .command('*')
-  .action(function(command){
-    console.log('command: "%s" not found', command);
-  });  
+function pagehandler(req, res, next){
+	var u = url.parse(req.url);
+	var file = u.pathname;
 
-program.parse(process.argv);
+	if(!file.match(/\.\w+?/)){
+		file += 'index.html';
+	}
+	else if(!file.match(/\.(html?|md)$/)){
+		next();
+		return;
+	}
 
-function runserver(port){
+	var markdownmode = false;
 
-	// create a spanking brand new Express App
-	// it will save Sessions into Redis and so will survive a crash/restart
+	function servecontent(content){
+		
 
-	var document_root = __dirname + '/www';
-
-	var app = express();
-	var server = http.createServer(app);
-
-	app.use(express.favicon());
-	app.use(express.query());
-	app.use(express.bodyParser());
-
-	app.engine('ejs', engines.ejs);
-	app.set('view engine', 'ejs');
-	app.set('views', __dirname + '/views');
-
-	function pagehandler(req, res, next){
-		var u = url.parse(req.url);
-		var file = u.pathname;
-
-		if(!file.match(/\.\w+?/)){
-			file += 'index.html';
-		}
-		else if(!file.match(/\.(html?|md)$/)){
-			next();
-			return;
-		}
-
-		var markdownmode = false;
-
-		function servecontent(content){
-			
-
-			res.render('layout', {
-				body:content
-			})
-		}
-
-		fs.readFile(document_root + file, 'utf8', function(error, content){
-			if(error || !content){
-				fs.readFile(document_root + file.replace(/\.html?$/, '.md'), 'utf8', function(error, content){
-					if(error || !content){
-						next();
-						return;
-					}
-					else{
-						markdownmode = true;
-						servecontent(content);
-					}
-				})
-				return;
-			}
-			else{
-				servecontent(content);
-			}
+		res.render('layout', {
+			body:content
 		})
 	}
 
-	app.use(pagehandler);
-
-	app.use(express.static(document_root));
-
-	server.listen(port, function(error){
-		console.log(dye.yellow('storytime island webserver listening on port: ') + dye.red(port));
+	fs.readFile(document_root + file, 'utf8', function(error, content){
+		if(error || !content){
+			fs.readFile(document_root + file.replace(/\.html?$/, '.md'), 'utf8', function(error, content){
+				if(error || !content){
+					next();
+					return;
+				}
+				else{
+					markdownmode = true;
+					servecontent(content);
+				}
+			})
+			return;
+		}
+		else{
+			servecontent(content);
+		}
 	})
 }
+
+app.use(pagehandler);
+
+app.use(express.static(document_root));
+
+server.listen(port, function(error){
+	console.log(dye.yellow('storytime island webserver listening on port: ') + dye.red(port));
+})
